c7768b593e1b3b5dd924fb7558a105d3424bfb6f
Olivier Gregoire <olivier.gregoire.ext@eurocontrol.int>
2019-07-22 10:02:34 +0000

    Fix problem 14

--- src/main/java/be/fror/projecteuler/problem/Problem14.java
+++ src/main/java/be/fror/projecteuler/problem/Problem14.java
@@ -6,36 +6,36 @@
 package be.fror.projecteuler.problem;
 
 /**
- *
  * @author Olivier Grégoire
  */
 public class Problem14 extends AbstractProblem {
 
-  @Override
-  public Object solve() {
-    int limit = 1000000;
-    int[] cache = new int[limit];
-    int maxChain = Integer.MIN_VALUE;
-    int maxValue = 0;
-    for (int n = 1; n < limit; n++) {
-      int chain = 1;
-      long t = n;
-      while (t != 1L && t >= n) {
-        if ((t & 1L) == 0L) {
-          t >>= 1;
-          chain++;
-        } else {
-          t = (3L * t + 1L) >> 1;
-          chain += 2;
+    @Override
+    public Object solve() {
+        int limit = 1_000_000;
+        int[] cache = new int[limit];
+        cache[1] = 1;
+        int maxChain = Integer.MIN_VALUE;
+        int maxValue = 0;
+        for (int n = 2; n < limit; n++) {
+            int chain = 0;
+            long t = n;
+            while (t != 1L && t >= n) {
+                if ((t & 1L) == 0L) {
+                    t >>= 1;
+                    chain++;
+                } else {
+                    t = (3L * t + 1L) >> 1;
+                    chain += 2;
+                }
+            }
+            cache[n] = chain + cache[(int) t];
+            if (cache[n] > maxChain) {
+                maxChain = cache[n];
+                maxValue = n;
+            }
         }
-      }
-      cache[n] = chain + cache[(int) t];
-      if (cache[n] > maxChain) {
-        maxChain = cache[n];
-        maxValue = n;
-      }
+        return maxValue;
     }
-    return maxValue;
-  }
 
 }
